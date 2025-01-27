f3bebb1e0dad5eb024cc858a1f794105c9eaf948
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-27 14:21:35 +0200

    installed semistandard npm module as a dev dependency and installed an exact version too using the appropriate npm install options --save-dev and --save-exact

--- consuming-modules/require-express.js
+++ consuming-modules/require-express.js
@@ -1,3 +1,5 @@
 // demo getting a 3rd party module from the npm registry and importing it into a program
+// installed semistandard module using: npm install --save-dev ---save-exact semistandard
+// run it with: ./node_modules/semistandard/bin/cmd.js
 const express = require('express');
 console.log('ran...');
